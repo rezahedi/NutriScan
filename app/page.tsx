156528fa75095280c64033a21a7cf2d7484e47cf
Reza Zahedi <r.zahedi@gmail.com>
2023-12-28 00:45:14 -0800

    QR image added to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
 
@@ -6,7 +7,11 @@ export default function Home() {
     <>
       <h1>Nutrition Facts Scanner App</h1>
       <p>This page is for showing off features of the application and tech stacks behind the app, link to github and screenshots.</p>
-      <Link href="/app" className="my-4 bg-accent text-background rounded-full p-4 px-8 shadow-xl">Application</Link>
+      <p>This application works only on mobile devices, because it uses the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Barcode_Detection_API"><u>Barcode Detection API</u></a> which is only available on mobile devices, and not on desktop browsers.</p>
+      <p>To use the application, you can either scan the QR code below, or click the link below it to open the application in your mobile browser.</p>
+      <Image src="/app-url-qr-code-dark.png" alt="App link QR code" width="300" height="300" className='bg-blend-difference' />
+      <p>&nbsp;</p>
+      <Link href="/app" className="my-4 bg-accent text-background rounded-full p-4 px-8 shadow-xl">Open Application</Link>
     </>
   );
 }
