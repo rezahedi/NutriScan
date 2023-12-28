85839a4db2ed9307930ada65c2619e4708e299cc
Reza Zahedi <r.zahedi@gmail.com>
2023-12-27 21:52:48 -0800

    missed component added to components index.ts

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,12 @@ import DeviceFrame from './DeviceFrame';
 import Scanner from './Scanner';
 import ProductCard from './ProductCard';
 import NutrientBar from './NutrientBar';
+import ProductsList from './ProductsList';
 
 export {
   DeviceFrame,
   Scanner,
   ProductCard,
   NutrientBar,
+  ProductsList
 }
