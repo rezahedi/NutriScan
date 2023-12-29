4116958d35e310ce5d9542176bc83af153883ef0
Reza Zahedi <r.zahedi@gmail.com>
2023-12-28 16:59:27 -0800

    NutrientBundle return null if nutrients empty

--- app/(app)/app/components/NutrientBundle.tsx
+++ app/(app)/app/components/NutrientBundle.tsx
@@ -2,6 +2,10 @@ import { NutrientBar } from "@/(app)/components";
 import { ProductNutrients } from "@prisma/client";
 
 export default function NutrientBundle( {title, nutrients}: {title: string, nutrients: ProductNutrients[]} ) {
+
+  if( nutrients.length === 0 )
+    return;
+
   return (
     <>
       <div className='flex justify-between items-center pt-6 pb-2'>
