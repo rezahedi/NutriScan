0ee0b6961333c911fcea02c66da35d81904fa97d
Reza Zahedi <r.zahedi@gmail.com>
2024-01-02 14:47:09 -0800

    refactor: api routes changed Changed from directly calling MongoDB data api endpoints to using actions from actions.ts actions used Prisma client to call MongoDB data api endpoints

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,47 +1,16 @@
+import { getProducts } from "@/(app)/actions";
 
 export async function GET(
   request: Request
 ) {
   try {
-    if ( process.env.MONGODB_API_KEY === undefined || process.env.MONGODB_DATA_API_BASE_URL === undefined )
-      throw new Error('Using .env.example as a template and create a .env file and add required env variables');
+    const searchParams = request.nextUrl.searchParams
+    const page = searchParams.get('page') || 1;
+    const limit = searchParams.get('limit') || 10;
 
-    const result = await fetch(`${process.env.MONGODB_DATA_API_BASE_URL}/action/find`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': process.env.MONGODB_API_KEY,
-        'Accept': 'application/ejson'
-      },
-      body: JSON.stringify(
-        {
-          "dataSource":"Cluster0",
-          "database":"nutrition-facts-scanner",
-          "collection": "Products",
-          "sort": {
-            "createdAt": -1
-          },
-          "limit": 10,
-          "projection": {
-            "_id": 0,
-            "barcode": 1,
-            "name": 1,
-            "image": 1,
-            "brandOwner": 1,
-            "brandName": 1,
-            "ingredients": 1,
-            "servingSize": 1,
-            "servingUnit": 1,
-            "packageWeight": 1,
-            "rated": 1,
-          }
-        }
-      )
-    });
-    
-    const json = await result.json();
-    return Response.json( json );
+    const result = await getProducts( parseInt( page ), parseInt( limit ) );
+
+    return Response.json( result || [] );
 
   } catch (error) {
     return new Response( JSON.stringify( {error} ), {
